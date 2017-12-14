8fbafce090af54973d563cbe4c16f01ac5b767af
Michael A. Gonzalez <genomics.geek.04.22@gmail.com>
2017-12-14 16:31:09 -0500

    Resolved #40

---
+++ src/lib/constants/genomics-urls.js
@@ -0,0 +1,41 @@
+// NCBI APIs
+export const NCBI_EUTILS_ENDPOINT = 'https://eutils.ncbi.nlm.nih.gov/entrez/eutils'
+export const NCBI_EFETCH_ENDPOINT = `${NCBI_EUTILS_ENDPOINT}/efetch.fcgi`
+export const NCBI_ESUMMARY_ENDPOINT = `${NCBI_EUTILS_ENDPOINT}/esummary.fcgi`
+export const NCBI_ESEARCH_ENDPOINT = `${NCBI_EUTILS_ENDPOINT}/esearch.fcgi`
+
+
+// NCBI URLs
+export const NCBI_URL = 'https://www.ncbi.nlm.nih.gov'
+export const PUBMED_URL = `${NCBI_URL}/pubmed`
+export const NCBI_VARIATION_URL = `${NCBI_URL}/variation/view`
+export const CLINVAR_URL = `${NCBI_URL}/clinvar`
+
+
+// Genomic Resources
+export const COSMIC_URL = 'http://cancer.sanger.ac.uk/cosmic'
+export const ENSEMBL_GRCH37_URL = 'http://grch37.ensembl.org'
+export const ENSEMBL_GRCH38_URL = 'http://grch38.ensembl.org'
+export const EXAC_URL = 'http://exac.broadinstitute.org'
+export const GNOMAD_URL = 'http://gnomad.broadinstitute.org'
+export const UCSC_URL = 'https://genome.ucsc.edu'
+
+
+// Gene Resources
+export const GENE_CARDS_URL = 'http://www.genecards.org'
+export const GHR_URL = 'https://ghr.nlm.nih.gov'  // Genetics Home Reference
+export const HGNC_URL = 'https://www.genenames.org'
+
+// Clinical Resources
+export const DECIPHER_URL = 'https://decipher.sanger.ac.uk'
+export const HGMD_URL = 'https://portal.biobase-international.com'
+export const OMIM_URL = 'https://www.omim.org'
+
+
+// Phenotype Resources
+export const HPO_URL = 'http://compbio.charite.de'
+export const ORPHANET_URL = 'http://www.orpha.net'
+
+
+// Internal Resources
+export const ALAMUT_URL = 'http://localhost:10000'
---
+++ src/lib/utils/urlBuilders.js
@@ -0,0 +1,98 @@
+import { padStart, startsWith, toLower, trimStart } from 'lodash'
+
+import * as urls from 'LibSrc/constants/genomics-urls'
+
+
+// NCBI APIs
+export const efetch = ( db, id, retmode = 'xml', params = '' ) =>
+  `${urls.NCBI_EFETCH_ENDPOINT}?retmode=${retmode}&db=${db}&id=${id}&${params}`
+
+export const esummary = ( db, id, retmode = 'json', params = '' ) =>
+  `${urls.NCBI_ESUMMARY_ENDPOINT}?retmode=${retmode}&db=${db}&id=${id}&${params}`
+
+export const esearch = ( db, term, retmode = 'json', params = '' ) =>
+  `${urls.NCBI_ESEARCH_ENDPOINT}?retmode=${retmode}&db=${db}&term=${term}&${params}`
+
+
+// NCBI URLs
+export const pubmedSearch = term => `${urls.PUBMED_URL}/?term=${term}`
+
+export const pubmedEntry = pmid => `${urls.PUBMED_URL}/${pmid}`
+
+export const variationViewer = ( chr, start, end, assembly = 'GCF_000001405.25') =>
+  `${urls.NCBI_VARIATION_URL}/?assm=${assembly}&chr=${chr}&from=${start}&to=${end}`
+
+export const clinvarSearch = term => `${urls.CLINVAR_URL}/?term=${term}`
+
+export const clinvarEntry = id => `${urls.CLINVAR_URL}/${id}`
+
+
+// Genomic URLs
+export const cosmicEntry = id => {
+  const trimmedId = trimStart(id, 'COSM')
+  return `${urls.COSMIC_URL}/mutation/overview?id=${trimmedId}`
+}
+
+export const ensemblLocus = ( chr, start, end, genome = 'grch37', species = 'Homo_sapiens') => {
+  let url
+
+  // TODO: This should be switched to a lookup object sicne genome and species are so related
+
+  if (['hg19', 'grch37'].includes(toLower(genome))) {
+    url = `${urls.ENSEMBL_GRCH37_URL}`
+  } else if (['hg38', 'grch38'].includes(toLower(genome))) {
+    url = `${urls.ENSEMBL_GRCH38_URL}`
+  } else {
+    throw new Error(`genome: ${genome} not supported!`)
+  }
+
+  return `${url}/${species}/Location/View?db=core;r=${chr}:${start}-${end}`
+}
+
+export const exacVariant = ( chr, pos, ref, alt ) =>
+  `${urls.EXAC_URL}/variant/${chr}-${pos}-${ref}-${alt}`
+
+export const gnomadVariant = ( chr, pos, ref, alt ) =>
+  `${urls.GNOMAD_URL}/variant/${chr}-${pos}-${ref}-${alt}`
+
+export const ucscLocus = ( chr, start, end, genome = 'hg19' ) =>
+  `${urls.UCSC}/cgi-bin/hgTracks?db=${genome}&position=chr${chr}:${start}-${end}`
+
+
+// Gene Resources
+export const geneCardsEntry = gene => `${urls.GENE_CARDS_URL}/cgi-bin/carddisp.pl?gene=${gene}`
+
+export const ghrEntry = gene => `${urls.GHR_URL}/gene/${gene}`
+
+export const hgncEntry = gene => `${urls.HGNC_URL}/cgi-bin/search?search_type=all&search=${gene}`
+
+
+// Clinical Resources
+export const decipherVariant = ( chr, start, end ) =>
+  `${urls.DECIPHER_URL}/search?q=${chr}:${start}-${end}#consented-patients/results`
+
+export const hgmdVariant = cdna => `${urls.HGMD_URL}/hgmd/pro/mut.php?acc=${cdna}`
+
+export const hgmdGene = gene => `${urls.HGMD_URL}/hgmd/pro/gene.php?gene=${gene}`
+
+export const omimEntry = id => `${urls.OMIM_URL}/entry/${id}`
+
+export const omimSearch = term => `${urls.OMIM_URL}/search/?index=entry&search=${term}`
+
+
+// Phenotype Resources
+export const hpoEntry = id => {
+  if (startsWith(id, 'HP:')) {
+    return `${urls.HPO_URL}/hpoweb/showterm?id=${id}`
+  } else {
+    const hpoId = padStart(id, 7, '0')
+    return `${urls.HPO_URL}/hpoweb/showterm?id=HP:${hpoId}`
+  }
+}
+
+export const orphanetEntry = id =>
+  `${urls.ORPHANET_URL}/consor/cgi-bin/OC_Exp.php?Lng=EN&Expert=${id}`
+
+
+// Internal Resources
+export const alamutEntry = cdna => `${urls.ALAMUT_URL}/show?request=${cdna}`
