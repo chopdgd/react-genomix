afa604f8c939cb8edb5d4be09dc025cb73e6d68b
Adam Gleason <gleasona@email.chop.edu>
2018-09-28 08:55:10 -0400

    Updated color choices for getInterpretationColor method

--- src/lib/utils/getInterpretationColor.js
+++ src/lib/utils/getInterpretationColor.js
@@ -1,8 +1,26 @@
-const getInterpretationColor = (classification) => {
+import { toLower } from 'lodash'
+
+
+const getInterpretationColor = value => {
+
+  const classification = toLower(value)
+
   switch (true) {
+
+    // Constitutional/Germline Classifications
     case classification.includes('pathogenic'):
       return 'red'
+    case ['uncertain significance', 'vus'].includes(classification):
+      return 'blue'
     case classification.includes('benign'):
+      return 'green'
+
+    // Somatic Classifications
+    case ['tier 1 - a', 'tier 1 - b', 'tier 2'].includes(classification):
+      return 'red'
+    case classification.includes('tier 3'):
+      return 'blue'
+    case classification.includes('tier 4'):
       return 'green'
     default:
       return 'black'
