1e15c43151c4ad19eeaf5b362e6df39f247f7f54
Mori N <noroozi.morteza@gmail.com>
2019-07-25 15:43:49 -0400

    Pass date format to dateFormatter (#481)
    Fixes moment deprecation warning
    
    Related to https://github.research.chop.edu/DGD/nexus/issues/1328

--- demo/src/components/ProgressBars.js
+++ demo/src/components/ProgressBars.js
@@ -8,57 +8,63 @@ const data = [
   {
     header: 'Done - On Time',
     target: 20,
-    start: "2018-01-01",
-    signout: "2018-01-21"
-  },{
+    start: '2018-01-01',
+    signout: '2018-01-21',
+  },
+  {
     header: 'Done - Late',
     target: 20,
-    start: "2018-01-01",
-    signout: "2018-01-25"
-  },{
+    start: '2018-01-01',
+    signout: '2018-01-25',
+  },
+  {
     header: 'Done - Early',
     target: 20,
-    start: "2018-01-01",
-    signout: "2018-01-15"
-  },{
+    start: '2018-01-01',
+    signout: '2018-01-15',
+  },
+  {
     header: 'Ongoing - On track',
     target: 30,
-    start: moment().add(-25, 'd').format('L'),
-    signout: null
-  },{
+    start: moment()
+      .add(-25, 'd')
+      .format('YYYY-MM-DD'),
+    signout: null,
+  },
+  {
     header: 'Ongoing - Behind Schedule',
     target: 27,
-    start: moment().add(-25, 'd').format('L'),
-    signout: null
-  },{
+    start: moment()
+      .add(-25, 'd')
+      .format('YYYY-MM-DD'),
+    signout: null,
+  },
+  {
     header: 'Ongoing - Ahead of Schedule',
     target: 60,
-    start: moment().add(-25, 'd').format('L'),
-    signout: null
-  }
+    start: moment()
+      .add(-25, 'd')
+      .format('YYYY-MM-DD'),
+    signout: null,
+  },
 ]
 
-const items = map(data, ({header,target,start,signout}) => ({
+const items = map(data, ({ header, target, start, signout }) => ({
   header: header,
-  description: <p>target: {target} <br/>start: {start} <br/>signout: {signout}</p>,
-  extra: (
-    <GenomiX.TurnAroundTimeProgress
-      target={target}
-      start={start}
-      signout={signout}
-    />
+  description: (
+    <p>
+      target: {target} <br />
+      start: {start} <br />
+      signout: {signout}
+    </p>
   ),
+  extra: <GenomiX.TurnAroundTimeProgress target={target} start={start} signout={signout} />,
 }))
 
 const ProgressBars = () => (
   <Grid padded centered>
     <Grid.Column width={16} textAlign="left">
-      <Card.Group
-        items={items}
-        textAlign="center"
-        itemsPerRow={3}
-        stackable
-      />
+      <Card.Group items={items} textAlign="center" itemsPerRow={3} stackable />
     </Grid.Column>
   </Grid>
 )
